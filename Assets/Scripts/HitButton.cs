e2c5856187148325442ed00916e62461e725eea7
Ichiro <1027952@etusd.org>
2019-03-15 17:55:24 -0700

    Animation?

---
+++ Assets/Scripts/HitButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitButton : MonoBehaviour {
+
+    public Transform wallTransform;
+    public Transform targetTransform;
+    public float health = 10f;
+    public float transformChange = 50f;
+	// Use this for initialization
+	void Start () {
+        targetTransform.GetComponent<Transform>();
+        wallTransform.GetComponent<Transform>();
+	}
+	
+    public void TakeDamage(float amount) {
+        health -= amount;
+        if(health <= 0f) {
+            health = 2f;
+            transformChange += 30f;
+
+        }
+    }
+
+
+}
